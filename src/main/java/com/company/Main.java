2af95928747be540395d328c29237ad78107204c
Stancu Mihai <stancumihai2000@yahoo.com>
2021-03-16 16:39:34 +0200

    fasfasf

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -14,6 +14,5 @@ public class Main {
         PolynomValidator polynomValidator = new PolynomValidator();
         PolyCalcController polyCalcController = new PolyCalcController(polyCalcView, polynomValidator, polyCalcModel);
         polyCalcView.setVisible(true);
-        System.out.println("trys");
     }
 }
